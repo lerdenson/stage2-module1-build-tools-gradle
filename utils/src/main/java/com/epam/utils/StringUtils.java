f2f544c8d796e2c6a3bee568009ca1bc1c293b4d
Максим <Maxim.d.morozov@gmail.com>
2023-07-13 21:38:56 +0400

    done?

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,7 +3,7 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          try {
-             int a = Integer.parseInt(str);
+             float a = Float.parseFloat(str);
              return a > 0;
          } catch (Exception e) {
              return false;
